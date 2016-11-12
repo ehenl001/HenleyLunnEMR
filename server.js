0571403161786988db8f2c2301446acbbb7e8179
Emmanuel Henley <ehenl001@fiu.edu>
2016-11-12 17:13:22 +0000

    Fixed some syntax errors.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*eslint-env node */
 /** 
 *Created:...................11/07/2016
 *Author:....................Emmanuel Henley
@@ -10,9 +11,9 @@
 *Server for the HenleyLunn EMR app
 **/
 
-var express = require('express');
+var express = require("express");
 var app = express();
-var http = require('http');
+var http = require("http");
 
 var host = "localhost";
 var port = 3030;
@@ -27,54 +28,54 @@ if (process.env.hasOwnProperty("VCAP_SERVICES")) {
   var port = process.env.VCAP_APP_PORT;	
   
   //Parse out Cloudant settings
-  cloudant = env['cloudantNoSQLDB'][0].credentials;
+  cloudant = env["cloudantNoSQLDB"][0].credentials;
 }
 
-var nano = require('nano')(cloudant.url);
-var db = nano.db.use('user_login');
+var nano = require("nano")(cloudant.url);
+var db = nano.db.use("user_login");
 
 //Set path to Jade template directory
-app.set('views', __dirname + '/views');
+app.set("views", __dirname + "/views");
 
 //Set path to Fonts directory
-app.set('fonts', __dirname + '/fonts');
+app.set("fonts", __dirname + "/fonts");
 
 //Set path to JavaScript files
-app.set('js', __dirname + '/js');
+app.set("js", __dirname + "/js");
 
 //Set path to CSS files
-app.set('css', __dirname + '/css');
+app.set("css", __dirname + "/css");
 
 //Set path to images files
-app.set('images', __dirname + '/images');
+app.set("images", __dirname + "/images");
 
 //Set path to static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //Bind the root '/' URL to the login page
-app.get('/', function(req, res){
-	res.render('login.jade', {title: 'Login'});
+app.get("/", function(req, res){
+	res.render("login.jade", {title: "Login"});
 });
 
 //Bind the '/patientExamination' URL to the drview page
-app.get('/patientExamination', function(req, res){
-	res.render('drView.jade', {title: 'Patient Examination'});
+app.get("/patientExamination", function(req, res){
+	res.render("drView.jade", {title: "Patient Examination"});
 });
 
 //Bind the '/patientVitals' URL to the nurseview page
-app.get('/patientVitals', function (req, res){
-	res.render('nurseView.jade', {title: 'Patient Pre-Examination'});
+app.get("/patientVitals", function (req, res){
+	res.render("nurseView.jade", {title: "Patient Pre-Examination"});
 });
 
 //Bind the '/scheduleAndBilling' URL to the receptionistView page
-app.get('/billing-and-scheduling', function (req, res){
-	res.render('receptionistView.jade', {title: 'Patient Billing and Scheduling'});
+app.get("/billing-and-scheduling", function (req, res){
+	res.render("receptionistView.jade", {title: "Patient Billing and Scheduling"});
 });
 
 var server = app.listen(port, function(){
-	console.log('Server running on port %d on host %s', server.address().port, host);
+	console.log("Server running on port %d on host %s", server.address().port, host);
 });
 
-process.on('exit', function(){
-	console.log('Server is shutting down!');
+process.on("exit", function(){
+	console.log("Server is shutting down!");
 });
